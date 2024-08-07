59945c4f56fc87180e3c675786e0e26da9ae182f
Ebba Þóra Hvannberg <ebba@hi.is>
2024-08-07 14:33:28 +0000

    Fyrsta útgáfa

---
+++ Vika1/HalloHeimur.java
@@ -0,0 +1,14 @@
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  : Prentar út Halló heimur á console
+ *
+ *
+ *****************************************************************************/
+
+public class HalloHeimur {
+    public static void main(String[] args) {
+        System.out.println("Halló heimur");
+    }
+}
