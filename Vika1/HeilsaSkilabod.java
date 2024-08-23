bb0d9686c7cf67c92447c63c6f8a80b107c7c4d2
Ebba Þóra Hvannberg <ebba@hi.is>
2024-08-23 14:14:15 +0000

    Ný dæmi

---
+++ Vika1/HeilsaSkilabod.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Nafn    : Nafn nemanda
+ *  T-póstur: xxx@hi.is
+ *
+ *  Lýsing  : Lýsing á hvað forritið gerir, inntak, útreikningar, úttak
+ *
+ *
+ *****************************************************************************/
+
+public class HeilsaSkilabod {
+    public static void main(String[] args) {
+        System.out.println ("Góðan daginn ");
+        System.out.println ("Gangi þér vel í dag ");
+    }
+}
---
+++ Vika1/ReiknaFormulu.java
@@ -0,0 +1,15 @@
+/******************************************************************************
+ *  Nafn    : Nafn nemanda
+ *  T-póstur: xxx@hi.is
+ *
+ *  Lýsing  : Lýsing á hvað forritið gerir, inntak, útreikningar, úttak
+ *
+ *
+ *****************************************************************************/
+
+public class ReiknaFormulu {
+    public static void main(String[] args) {
+        System.out.print("(10.5 + 2 * 3) / (45 - 3.5) = ");
+        System.out.println((10.5 + 2 * 3) / (45 - 3.5));
+    }
+}
