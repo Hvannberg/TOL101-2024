918af2770ac71691a14055cadf50512cd13e926b
Ebba Þóra Hvannberg <ebba@hi.is>
2024-10-22 15:55:06 +0000

    uppfærsla - fleiri dæmi

---
+++ Vika10/10.1/TestCircleRectangle.java
@@ -0,0 +1,25 @@
+/******************************************************************************
+ *  Nafn    : Ebba Þóra Hvannberg
+ *  T-póstur: ebba@hi.is
+ *
+ *  Lýsing  : Client fyrir notkun á Circle og Rectangle
+ *
+ *
+ *****************************************************************************/
+
+public class TestCircleRectangle {
+    public static void main(String[] args) {
+        Circle circle = new Circle(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getRadius()*2);
+
+        Rectangle rectangle = new Rectangle(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " +
+                                   rectangle.getPerimeter());
+    }
+}
